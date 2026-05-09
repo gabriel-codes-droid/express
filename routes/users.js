d356081c1dec8b7e954e5be9e0e68dd0bee859ef
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-05-09 21:13:57 +0200

    .

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,21 @@ res.send ("User list")
 router.get('/new',(req,res)=>{
 res.send ("User New form")
 })
+
+router.post('/',(req,res)=>{
+    res.send ('Create User')
+})
+router
+.route("/:id")
+.get((req,res)=>{
+    res.send(`Get User With ID ${req.params.id}`)
+})
+.put((req,res)=>{
+    res.send(`Update User With ID ${req.params.id}`)
+})
+.delete((req,res)=>{
+    res.send(`Delete User With ID ${req.params.id}`)
+})
+
+
 module.exports = router;
