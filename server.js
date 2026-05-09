0bc80c462c056a130a61457251c81ba131cdce4b
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-05-09 22:06:43 +0200

    .

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
-app.set('view engine','ejs')
+app.use(express.static('public'))
+// app.set('view engine','ejs')
+app.use(logger)
 app.get('/',(req,res,)=>{
     console.log('Here');
     // res.status(200).send('Hi');
     // res.download("server.js");
-    res.render("index",{ text:' World'})
+   // res.render("index",{ text:' World'})
    
 })
 
-
 const userRouter = require('./routes/users')
+
 app.use('/users',userRouter)
+
+function logger(req,res,next){
+    console.log(req.originalUrl)
+    next()
+}
+
 app.listen(3000);
